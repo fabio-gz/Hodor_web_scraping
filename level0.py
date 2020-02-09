0396a3962137d065e74d00b7e354971daf40286f
fabio <fabiogomez.silva@gmail.com>
2020-02-09 18:45:11 -0500

    Votes 1024 times for the 1215 id

---
+++ level0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""votes 1024 times for the id 1215"""
+import requests
+
+url = "http://158.69.76.135/level0.php"
+payload = {'id': '1215', 'holdthedoor': 'Submit'}
+
+for i in range(1024):
+    r= requests.post(url, data=payload)
+
+if r.status_code == requests.codes.ok:
+    print('Sucess')
